11d2411d217ad251f9c10ab573be44efa633ca51
Jackson Waskowitz <jacksonwaskowitz@Jacksons-MacBook.local>
2017-01-02 17:42:35 -0500

    added alerts

--- main.js
+++ main.js
@@ -5,3 +5,12 @@ var cardTwo = 'king'
 var cardThree = 'queen'
 var cardFour = 'queen'
 
+if (cardOne === cardTwo) {
+	alert('You found a match!');
+}
+if (cardThree === cardFour) {
+	alert('You found a match!');
+}
+else {
+	alert('Sorry, try again.');
+}
