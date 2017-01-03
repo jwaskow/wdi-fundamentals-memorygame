a068c27de1a3192f87fa33981257b5461a9b3869
Jackson Waskowitz <jacksonwaskowitz@Jacksons-MacBook.local>
2017-01-03 14:56:13 -0500

    removed unused js code

--- main.js
+++ main.js
@@ -40,19 +40,5 @@ function isMatch(cards) {
 }
 
 createBoard();
-/*
-var cardOne = 'king'
-var cardTwo = 'king'
-var cardThree = 'queen'
-var cardFour = 'queen'
 
-if (cardOne === cardTwo) {
-	alert('You found a match!');
-}
-if (cardThree === cardFour) {
-	alert('You found a match!');
-}
-else {
-	alert('Sorry, try again.');
-}*/
 
